882bfd39e3191802c28c2591360bd26c253a5a3a
OneCrazyman <cjw7155@naver.com>
2024-09-02 02:26:41 +0900

    #

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,40 +1,77 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/layout';
-import PostList from '../components/post-list';
+import * as React from "react"
+import { Link, graphql } from "gatsby"
 
-const CategoryTemplate = ({ data, pageContext }) => {
-  const { category } = pageContext;
-  const posts = data.allMarkdownRemark.edges;
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const CategoryTemplate = ({
+  data: { allMarkdownRemark, site },
+  pageContext,
+  location,
+}) => {
+  const siteTitle = site.siteMetadata?.title || `Title`
+  const posts = allMarkdownRemark.nodes
+  const { category } = pageContext
 
   return (
-    <Layout>
-      <h1>{category}</h1>
-      <PostList posts={posts} />
+    <Layout location={location} title={siteTitle}>
+      <Seo title={`${category} 카테고리`} />
+      <h1>{category} 카테고리</h1>
+      <ul>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug
+          return (
+            <li key={post.fields.slug}>
+              <article itemScope itemType="http://schema.org/Article">
+                <header>
+                  <h2>
+                    <Link to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>{post.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: post.frontmatter.description || post.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          )
+        })}
+      </ul>
     </Layout>
-  );
-};
+  )
+}
+
+export default CategoryTemplate
 
 export const pageQuery = graphql`
-  query($category: String) {
+  query CategoryPage($category: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     allMarkdownRemark(
-      filter: { fields: { category: { eq: $category } } }
-      sort: {frontmatter: {date: DESC}}
+      filter: { frontmatter: { category: { eq: $category } } }
+      sort: { frontmatter: { date: DESC } }
     ) {
-      edges {
-        node {
-          frontmatter {
-            title
-            date(formatString: "MMMM DD, YYYY")
-          }
-          fields {
-            slug
-          }
-          excerpt
+      nodes {
+        fields {
+          slug
         }
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+          description
+        }
+        excerpt
       }
     }
   }
-`;
-
-export default CategoryTemplate;
+`
